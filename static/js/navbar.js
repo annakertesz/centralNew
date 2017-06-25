9da6b03090b51cd117128583e726726cfa3ee410
annakertesz <kerteszannanak@gmail.com>
2017-06-25 16:22:36 +0200

    homepage

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -5,20 +5,27 @@
  var hide_all = function () {
         $("#browser").hide();
         $("#playlists").hide();
+        $("#home").hide();
+    };
+
+    var show_home = function () {
+        hide_all()
+        $("#home").show();
     };
     
     var show_browser = function () {
         hide_all();
-        $("#browser").show()
+        $("#browser").show();
     };
     
     var show_playlists = function () {
         hide_all();
-        $("#playlists").show()
+        $("#playlists").show();
     };
 
 $(document).ready(function() {
     
 
     hide_all();
+    show_home();
 });
