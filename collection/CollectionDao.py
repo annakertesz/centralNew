8f24787c527418b2f755f937c2eb2fe1924fb20f
Matyas Forian Szabo <forianszm@gmail.com>
2017-08-21 00:29:51 +0200

    work on multi upload

--- collection/CollectionDao.py
+++ collection/CollectionDao.py
@@ -1,6 +1,5 @@
 import PIL
 import io
-
 from django.utils.text import slugify
 
 from central_publishing_new.settings import MEDIA_ROOT
@@ -8,27 +7,23 @@ from collection.models import Album, Artist, Song, Tag
 import eyed3
 from eyed3.utils import art
 from PIL import Image
-import base64
+
 
 def add_song(file_name):
     # TODO: iterate through albums and artists
     print("ADD SONG")
     song = eyed3.load(MEDIA_ROOT + '/' + file_name)
-    # if song.tag.album == None or song.tag.artist == None or song.tag.title != None:
-    #     print("missing arg:")
-        # print(song.tag.album + " " + song.tag.artist + " " + song.tag.title)
-        # TODO: delete files
-    # else:
     try:
         artist = Artist.objects.get(artist_name=song.tag.artist)
     except Artist.DoesNotExist:
         artist = Artist(artist_name=song.tag.artist)
         artist.save()
 
-
     try:
-       album = Album.objects.get(album_name=song.tag.album)
+        album = Album.objects.get(album_name=song.tag.album)
     except Album.DoesNotExist:
+        album = Album(album_name=song.tag.album, artist=artist, cover=slugify(song.tag.album))
+        album.save()
 
         images = art.getArtFromTag(song.tag)
         # image_tag = ''  # default
@@ -40,12 +35,9 @@ def add_song(file_name):
             # hsize = int((float(img.size[1]) * float(wpercent)))
             img = img.resize((300, 300), PIL.Image.ANTIALIAS)
             img.save(MEDIA_ROOT+"/covers/"+ slugify(song.tag.album)+".jpg")
-        album = Album(album_name=song.tag.album, artist=artist, cover=slugify(song.tag.album))
-        album.save()
 
     song = Song(name=song.tag.title, artist=artist, album=album, path=file_name)
     song.save()
-
 
 
 def simple_search(string, isExact):
