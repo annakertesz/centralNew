5eb6635bca58c8c85ee9c95ed3bde037e7cabeb0
Anna-Kertesz <anna.kertesz@ibm.com>
2019-09-23 17:02:27 +0200

    baseroot

--- central_publishing_new/settings.py
+++ central_publishing_new/settings.py
@@ -21,7 +21,7 @@ from django.conf import settings
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ADMIN_EMAIL = "kerteszannanak@gmail.com"
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
